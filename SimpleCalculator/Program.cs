aa81e3e65fb2829e29579d8cb06506488768ad8d
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-02 08:56:44 +0200

    Implemented four basic operations +, -, *, /

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,7 +6,27 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                Console.WriteLine("Enter first number: ");
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter second number: ");
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter the operator: ");
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Start logging exceptions
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
